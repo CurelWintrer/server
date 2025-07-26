92149860a11c049c14cc79a6dd44aec42ebbc360
WinterCruel <78720290+CurelWintrer@users.noreply.github.com>
2025-07-26 18:24:03 +0800

    token有效时长

--- utils/auth.js
+++ utils/auth.js
@@ -4,7 +4,7 @@ function generateToken(user) {
   return jwt.sign(
     { userID: user.userID, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: '1h' }
+    { expiresIn: '24h' }
   );
 }
 
